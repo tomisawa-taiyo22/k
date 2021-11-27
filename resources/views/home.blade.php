c3482ddbddb0bc3c3926b1977b7b300e8e310b55
github <github>
2021-11-27 09:46:37 +0900

    日本語にする

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
-@extends('layouts.app')
-
+@extends('layouts.common')
+@section('title', 'ログイン完了')
 @section('content')
 <main class="sm:container sm:mx-auto sm:mt-10">
     <div class="w-full sm:px-6">
@@ -13,12 +13,12 @@
         <section class="flex flex-col break-words bg-white sm:border-1 sm:rounded-md sm:shadow-sm sm:shadow-lg">
 
             <header class="font-semibold bg-gray-200 text-gray-700 py-5 px-6 sm:py-6 sm:px-8 sm:rounded-t-md">
-                Dashboard
+                通知
             </header>
 
             <div class="w-full p-6">
                 <p class="text-gray-700">
-                    You are logged in!
+                    ログインが完了しました!
                 </p>
             </div>
         </section>
