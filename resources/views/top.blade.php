dc7c3566bb8bc026dc4dc367afa8cde8bf82d48a
github <github>
2021-11-28 22:48:21 +0900

    トップ画面の作成

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,7 +1,19 @@
 @extends('layouts.common')
 @section('title', 'Kanban-Board')
 @section('content')
-<div id="app">
-    <example-component></example-component>
-</div>
+<main class='container mx-auto'>
+    <div>
+        <h1 class="text-9xl text-green-700 font-semibold">Kanban-Board</h1>
+    </div>
+    <div>
+        <button
+            class="text-4xl bg-blue-700 border border-blue-500 px-6 py-2 text-white py-2 px-4  rounded"
+        >
+        <a href="{{ url('/register') }}">
+            {{ ('ユーザー登録') }}
+        </a>
+    </div>
+</button>
+</main>
+
 @endsection
